0751840b12cc36b1357f1682732dba9a18cff938
Vikram Kakkar <vikram.kakkar@thescore.com>
2022-08-25 20:39:25 -0400

    - await

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {wait} from './wait'
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    /*const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', new Date().toTimeString())*/
 
     // const teams: string = 'owner-team'
     const teamMembers = await github
