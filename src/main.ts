4bd17186c8e293a4c95034f8e37f6cb566cfc902
Vikram Kakkar <vikram.kakkar@thescore.com>
2022-08-25 20:21:46 -0400

    init - get team members

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -11,6 +12,15 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+
+    // const teams: string = 'owner-team'
+    const teamMembers = github
+      .getOctokit('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')
+      .rest.teams.getByName({
+        org: 'appeaser',
+        team_slug: 'owner-team'
+      })
+    core.debug(`team members: ${teamMembers}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
